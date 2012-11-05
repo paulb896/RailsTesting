36ef7df48845a843d739d611faf162d58d8a351c
paulb896 <paulb896@gmail.com>
2012-11-05 10:52:18 -0700

    Add base rails application

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 2. Add functional requirement (bdd test(s)) to gem using RSpec.
 3. Make test(s) pass.
 4. Create test(s) to cover previously created functionality.
-5. Add gem to simple rails blog app ($ rails new blog)
+5. Add gem to simple rails blog app ($ rails new TestingApp)
 6. Incorporate new functionality into blogger.
 7. Add cucumber tests that incorporate a controller, view, and model.
 8. Make cucumber tests pass.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingApp::Application.config.secret_token = '904689c633eadd06a107085ad1ea026ea9821031627493eab2aed75f58427b933680b08518ee4843bcbc7eff37dd80c81b1cd82939b09aea2fc8b68311751aec'
